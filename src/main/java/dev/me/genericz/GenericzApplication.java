6767e56728fb0a7cdfe95dca027d4dbae2b894e0
bohdan167 <malanka321@gmail.com>
2025-03-11 16:24:06 +0000

    change package manager Maven to Gradle

---
+++ src/main/java/dev/me/genericz/GenericzApplication.java
@@ -0,0 +1,13 @@
+package dev.me.genericz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericzApplication.class, args);
+	}
+
+}
--- src/main/java/dev/me/genericz/Person.java
+++
@@ -1,6 +0,0 @@
-package dev.me.genericz;
-
-import org.springframework.data.annotation.Id;
-
-record Person(@Id Long id, String name, Integer age, String username) {
-}
